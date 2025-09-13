a7c154128ed6df783c596734896c4ef506b64646
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-09-13 03:04:30 -0300

    Configurando o OpenAPI e Swagger

---
+++ src/main/java/br/com/fiap/fiapvideos/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package br.com.fiap.fiapvideos.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    OpenAPI custonOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
+                .info(new Info()
+                        .title("API Autenticação")
+                        .description("Documentação da API")
+                        .version("1.0"))
+                .info(new Info()
+                        .title("API Autenticação")
+                        .description("Documentação da API")
+                        .version("1.0"))
+                .security(Arrays.asList(new SecurityRequirement().addList("bearerAuth")));
+    }
+
+}
