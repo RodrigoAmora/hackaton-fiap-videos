fa48e7e79eeaa720788fda32058e9991e05cf0eb
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-09-13 02:51:38 -0300

    Commit do projeto

---
+++ src/test/java/br/com/fiap/fiapvideos/FiapVideosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiapvideos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapVideosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
