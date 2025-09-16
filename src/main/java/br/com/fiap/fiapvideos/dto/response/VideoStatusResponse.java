09d2774e7c2b7970ec8c296cbd4d1e91e9d20928
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-09-16 01:10:29 -0300

    Adicionando ownerId e fileName

--- src/main/java/br/com/fiap/fiapvideos/dto/response/VideoStatusResponse.java
+++ src/main/java/br/com/fiap/fiapvideos/dto/response/VideoStatusResponse.java
@@ -2,7 +2,9 @@ package br.com.fiap.fiapvideos.dto.response;
 
 public record VideoStatusResponse(
         Long id,
+        String ownerId,
         String status,
+        String fileName,
         String resultZipPath,
         String errorMessage
 ) {}
