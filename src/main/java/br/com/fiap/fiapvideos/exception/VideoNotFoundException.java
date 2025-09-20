f66e957d5cbb36ed47a0ba3ca3799a16a90e910a
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-09-20 17:20:21 -0300

    Print da cobertura do testes do projeto

--- src/main/java/br/com/fiap/fiapvideos/exception/VideoNotFoundException.java
+++ src/main/java/br/com/fiap/fiapvideos/exception/VideoNotFoundException.java
@@ -3,10 +3,10 @@ package br.com.fiap.fiapvideos.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)
-public class VideoException extends RuntimeException {
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class VideoNotFoundException extends RuntimeException {
 
-    public VideoException(String message) {
+    public VideoNotFoundException(String message) {
         super(message);
     }
 
