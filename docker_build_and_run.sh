ce89b700613023c9a52a3c87747675f1598df34c
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-09-21 00:27:11 -0300

    Shell script para subir os containers do Docker

--- docker_build_and_run.sh
+++ docker_build_and_run.sh
@@ -29,6 +29,7 @@ if [[ $(uname -s) == "Darwin" ]]; then
   echo "Running on MacOS, forcing linux/amd64"
   echo -e "\n"
 
+  echo -e "\n"
   echo -e "\033[01;32m### Building images.... ###\033[01;32m"
   echo -e "\n"
 
@@ -45,17 +46,18 @@ else
   echo "Running on Linux/Windows"
   echo -e "\n"
 
+  echo -e "\n"
   echo -e "\033[01;32m### Building images.... ###\033[01;32m"
   echo -e "\n"
 
   sudo docker-compose build --no-cache
 
+  echo -e "\n"
   echo -e "\033[01;32m### Upping containers ###\033[01;32m"
   echo -e "\n"
 
   sudo docker-compose up -d --force-recreate
 fi
-
 
 echo -e "\n"
 echo -e "\033[01;32m###############################\033[01;32m"
