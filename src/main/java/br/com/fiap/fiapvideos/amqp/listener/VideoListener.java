ab2d57a62e3c4b30a4479c0c19a5b00a2cb7a47b
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-09-26 23:10:25 -0300

    Tratamento para não receber mensagem duplicada

--- src/main/java/br/com/fiap/fiapvideos/amqp/listener/VideoListener.java
+++ src/main/java/br/com/fiap/fiapvideos/amqp/listener/VideoListener.java
@@ -7,13 +7,18 @@ import br.com.fiap.fiapvideos.repository.VideoRepository;
 import br.com.fiap.fiapvideos.service.NotificationService;
 import br.com.fiap.fiapvideos.util.VideoUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.concurrent.TimeUnit;
+
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class VideoListener {
 
     @Autowired
@@ -25,36 +30,45 @@ public class VideoListener {
     @Autowired
     private NotificationService notificationService;
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
     @RabbitListener(queues = "video.process.queue")
     public void receive(VideoMessage message) {
-        Long videoId = message.videoId();
-        String userEmail = message.userEmail();
+        String processKey = "video_process:" + message.videoId();
 
-        Video video = findVideoById(videoId);
-        video.setStatus(VideoStatus.PROCESSING);
+        if (redisTemplate.opsForValue().setIfAbsent(processKey, "processing", 1, TimeUnit.HOURS)) {
+            Long videoId = message.videoId();
+            String userEmail = message.userEmail();
 
-        updatedStatusVideo(video);
+            Video video = findVideoById(videoId);
+            video.setStatus(VideoStatus.PROCESSING);
 
-        // Processa o vídeo
-        boolean success = processVideo(String.valueOf(videoId), message.path(), message.prefixFileName());
-        System.out.println("Result ---> " + success);
-        if (success) {
-            var resultZipPath = VideoUtil.VIDEO_FILE_OUTPUT_DIR + "/"+ videoId + ".zip";
-            video.setResultZipPath(resultZipPath);
-            video.setStatus(VideoStatus.DONE);
             updatedStatusVideo(video);
 
-            System.out.println("Worker: updated status to DONE for video " + videoId);
+            // Processa o vídeo
+            boolean success = processVideo(String.valueOf(videoId), message.path(), message.prefixFileName());
+            System.out.println("Result ---> " + success);
+            if (success) {
+                var resultZipPath = VideoUtil.VIDEO_FILE_OUTPUT_DIR + "/" + videoId + ".zip";
+                video.setResultZipPath(resultZipPath);
+                video.setStatus(VideoStatus.DONE);
+                updatedStatusVideo(video);
 
-            notificationService.notifySuccess(userEmail, videoId);
+                System.out.println("Worker: updated status to DONE for video " + videoId);
+
+                notificationService.notifySuccess(userEmail, videoId);
+            } else {
+                video.setErrorMessage("Erro ao processar o vídeo");
+                video.setStatus(VideoStatus.FAILED);
+                updatedStatusVideo(video);
+
+                System.out.println("Worker: updated status to FAILED for video " + videoId);
+
+                notificationService.notifyError(userEmail, videoId);
+            }
         } else {
-            video.setErrorMessage("Erro ao processar o vídeo");
-            video.setStatus(VideoStatus.FAILED);
-            updatedStatusVideo(video);
-
-            System.out.println("Worker: updated status to FAILED for video " + videoId);
-
-            notificationService.notifyError(userEmail, videoId);
+            log.warn("Mensagem duplicada recebida para o vídeo: {}", message.videoId());
         }
     }
 
